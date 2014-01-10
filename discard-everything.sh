4e227ead031d4c5a72b6a61ea76e84717c909c2e
Stefan Tauner <stefan.tauner@gmx.at>
2014-01-10 11:48:41 +0100

    Work around missing blocking lvremove

--- discard-everything.sh
+++ discard-everything.sh
@@ -23,8 +23,18 @@ done
 # then run blkdiscard on any free discardable space covered by lvm
 pvs -o pv_name,vg_name,vg_free_count --noheadings | while read dev vgname free ; do
 	if [ -b "$dev" ] && [ $(blockdev  --getdiscardzeroes $dev) -eq 0 ] && [ "$free" -gt 0 ]; then
-		lvcreate -l100%FREE -n discard ${vgname} && blkdiscard -v  /dev/${vgname}/discard
-		lvchange -f -an /dev/${vgname}/discard
-		lvremove -f /dev/${vgname}/discard
+		lvcreate -l100%FREE -n discard ${vgname} >/dev/null && blkdiscard /dev/${vgname}/discard
+		# There are (or at least were) many bugs prohibiting a clean removal
+		# see http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=718582
+		# https://bugzilla.redhat.com/show_bug.cgi?id=715624 etc.
+		# The least invasive recovery strategy is just retrying a few times, so...
+		cnt=5
+		while [ $cnt -gt 0 ] && ! lvremove -f /dev/${vgname}/discard >/dev/null 2>&1 ; do
+			sleep 1
+			cnt=$(($cnt-1))
+		done
+		if [ "$cnt" -eq 0 ]; then
+			echo "Could not remove logical volume /dev/${vgname}/discard" >&2
+		fi
 	fi
 done
